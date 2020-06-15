ebabb3935fb982aff77a2c7b4b289abb9b0af09f
LAMZA <lamzahk@yahoo.com.hk>
2020-06-15 14:36:22 +0900

    Commit UnityStudy Files.

---
+++ StudioOops/Assets/UI_Study_COMA/Script/COMA.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COMA : MonoBehaviour
+{
+    public GameObject TitleMenu;
+    public GameObject Options;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TitleMenu.SetActive(true);
+        Options.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void CallOptions()    {        Invoke("CallOptions2", 0.2f);
+        //TitleMenu.SetActive(false);
+        //Options.SetActive(true);
+    }    public void CallOptions2()    {
+        //  invoke(CallOptions2, 0.2f);
+        TitleMenu.SetActive(false);        Options.SetActive(true);    }    public void CallTitleMenu()    {        Invoke("CallTitleMenu2", 0.2f);
+        //  TitleMenu.SetActive(true);
+        //Options.SetActive(false);
+        Debug.Log("koko");    }    public void CallTitleMenu2()    {
+        //Invoke("CallTitleMenu", 0.2f);
+        TitleMenu.SetActive(true);        Options.SetActive(false);        Debug.Log("koko");    }
+}
---
+++ StudioOops/Assets/UI_Study_COMA/Script/MenuButtonController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonController : MonoBehaviour
+{
+    public int index;
+    [SerializeField] bool KeyDown;
+    [SerializeField] int maxIndex;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
